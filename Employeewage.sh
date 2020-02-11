3cc74287c7407ce37062f489f66b0b973638b80b
siddhivinayak08 <siddhivinup@gmail.com>
2020-02-11 17:21:28 +0530

    [imran] added dailyWagsfun

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,2 +1,18 @@
 echo "Welcome to Employee wage"
-echo "Hello"
+function attend()
+{
+rnd=$(( RANDOM%2 ))
+if [ $rnd -eq 1 ]
+ then 
+   echo "Employee is present "
+ else 
+      echo "Employee is absent "
+  fi
+}
+function dailyWages()
+{
+ d=$(( 8*20 ))
+echo $d
+}
+attend
+dailyWages
